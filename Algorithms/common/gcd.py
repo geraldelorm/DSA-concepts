81782f5e4702fc5db8b4698b45cc496e055dd1d3
Gerald Elorm Gbagbe <gerald.gbagbe@turntabl.io>
2022-05-30 23:59:42 +0000

    GCD implementation

---
+++ Algorithms/common/gcd.py
@@ -0,0 +1,22 @@
+# Naive Solution
+def gcd(numerator, denominator): # greatest common divisor
+    best = 0
+    for x in range(1, numerator + denominator):
+        if numerator % x == 0 and denominator % x == 0:
+            best = x
+    return best
+
+# Time Complexity = O(n)  -- n + m
+# Space Complexity = o(1)
+
+
+# Optimal Solution
+def gcd(numerator, denominator): # greatest common divisor
+    if denominator == 0:
+        return numerator
+    numPrime = numerator % denominator
+    return gcd(denominator, numPrime)
+
+print(gcd(357,234))
+# Time Complexity = O(logn)
+# Space Complexity = o(1)
