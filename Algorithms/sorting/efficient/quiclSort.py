98f56c9fb56cebe0d30b56269853a6d13de58cea
Gerald Elorm Gbagbe <gerald.elorm.gbagbe@gmail.com>
2022-07-17 18:11:51 +0000

    quick sort implementation

---
+++ Algorithms/sorting/efficient/quiclSort.py
@@ -0,0 +1,19 @@
+def quickSort(arr):
+    if len(arr) < 2:
+        return arr
+    pivot = arr[0]
+    leftLess = [i for i in arr[1:] if i <= pivot]
+    rightGreater = [i for i in arr[1:] if i > pivot]
+
+    return quickSort(leftLess) + [pivot] + quickSort(rightGreater) #tail recursion 😜
+
+
+
+# Time = O(nlogn)
+# Space = O(1)
+
+# TEST
+assert quickSort([5, 3, 4, 2, 1]) == [1, 2, 3, 4, 5]
+assert quickSort([98, 56, 87, 45]) == [45, 56, 87, 98]
+
+print("Passed ✔")
